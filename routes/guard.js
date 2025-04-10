d2687cf224e5efa0a8202a4ba2bbc92552c7e679
GooseThomas <ok.nikiforov@gmail.com>
2025-04-10 14:35:42 +0500

    secont commit

--- routes/guard.js
+++ routes/guard.js
@@ -2,6 +2,6 @@ const {Router} = require('express')
 const router = Router()
 const pl = require('../controllers/guard')
 //GET, POST, PUT, PATCH, and DELETE
-router.get  ('/info'                     , pl.root         )
-router.stack.forEach(t=>{ console.log('[pl]', t.route.path)})
+router.get  ('/info'                     , pl.info         )
+router.stack.forEach(t=>{ console.log('[api]', t.route.path)})
 module.exports = router
